d3247a6d1948cbddcb8a33fd00116c6614f6573e
Mohamed Aneddame <maneddam@e1r1p10.1337.ma>
2023-11-30 12:19:18 +0100

    done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maneddam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 17:13:15 by maneddam          #+#    #+#             */
-/*   Updated: 2023/11/30 11:57:57 by maneddam         ###   ########.fr       */
+/*   Updated: 2023/11/30 12:15:45 by maneddam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int main()
         B b; 
         C c;
 
-        identify( base);
+        identify(base);
         std::cout << std::endl;
-        identify( a);
-        identify( b);
-        identify( c);
+        identify(a);
+        identify(b);
+        identify(c);
         delete base;
     }
     catch(std::exception& e)
